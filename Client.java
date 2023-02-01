4552c4df1e0a42e559bd8330d350234c60ae6818
klebersalllum <klebersall@gmail.com>
2023-02-01 10:03:11 -0300

    commit

---
+++ Client.java
@@ -0,0 +1,11 @@
+public class Client extends Person {
+    public Client(String name, String email, String password) {
+        super(name, email, password);
+    }
+
+    @Override
+    public void performTask() {
+        // TODO Auto-generated method stub
+        
+    }
+}
---
+++ Employee.java
@@ -0,0 +1,15 @@
+public class Employee extends Person {
+    public Employee(String name, String email, String password) {
+        super(name, email, password);
+    }
+
+    public void confirmOrder() {
+        // Confirm customer's order
+    }
+
+    @Override
+    public void performTask() {
+        // TODO Auto-generated method stub
+        
+    }
+}
